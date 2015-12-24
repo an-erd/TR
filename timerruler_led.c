d64d45942528294be0d898ad139d96261996ac1b
an-erd <anerd@gmx.de>
2015-12-24 13:01:46 +0100

    cosmetic

--- timerruler_led.c
+++ timerruler_led.c
@@ -17,45 +17,45 @@ const uint8_t ledArray[NUM_LEDS][2] = {
 	{4, 4}, {4, 5}, {4, 6}, {4, 7},
 	{2, 1}, };
 
-	// "cascade" and "2-on effect", only PORTD, PD5..7 resp. PD0..4
-	const uint8_t	ledArrayOrangeCascade_LR[4]	= { 0x00, 0x20, 0x60, 0xE0, };
-	const uint8_t	ledArrayOrangeEffect[5][2]	= { {0, 0}, {0x80, 0x20}, {0xC0, 0x60}, {0x60, 0xC0}, {0x20, 0x80}, };
-	const uint8_t	ledArrayRedCascade_LR[6]	= { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, };
+// "cascade" and "2-on effect", only PORTD, PD5..7 resp. PD0..4
+const uint8_t	ledArrayOrangeCascade_LR[4]	= { 0x00, 0x20, 0x60, 0xE0, };
+const uint8_t	ledArrayOrangeEffect[5][2]	= { {0, 0}, {0x80, 0x20}, {0xC0, 0x60}, {0x60, 0xC0}, {0x20, 0x80}, };
+const uint8_t	ledArrayRedCascade_LR[6]	= { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, };
 
-	// define some specific LED operations
-	void led_set (uint8_t led_nr, uint8_t led_state)
+// define some specific LED operations
+void led_set (uint8_t led_nr, uint8_t led_state)
+{
+	switch (ledArray[led_nr][0])
 	{
-		switch (ledArray[led_nr][0])
-		{
-			case 2: bit_write(led_state, PORTB, BIT(ledArray[led_nr][1])); break;
-			case 3: bit_write(led_state, PORTC, BIT(ledArray[led_nr][1])); break;
-			case 4: bit_write(led_state, PORTD, BIT(ledArray[led_nr][1])); break;
-			default: ;
-		}
-		return;
+		case 2: bit_write(led_state, PORTB, BIT(ledArray[led_nr][1])); break;
+		case 3: bit_write(led_state, PORTC, BIT(ledArray[led_nr][1])); break;
+		case 4: bit_write(led_state, PORTD, BIT(ledArray[led_nr][1])); break;
+		default: ;
 	}
+	return;
+}
 
-	void led_flip (uint8_t led_nr)
+void led_flip (uint8_t led_nr)
+{
+	switch (ledArray[led_nr][0])
 	{
-		switch (ledArray[led_nr][0])
-		{
-			case 2: bit_flip(PORTB, BIT(ledArray[led_nr][1])); break;
-			case 3: bit_flip(PORTC, BIT(ledArray[led_nr][1])); break;
-			case 4: bit_flip(PORTD, BIT(ledArray[led_nr][1])); break;
-			default: ;
-		}
-		return;
+		case 2: bit_flip(PORTB, BIT(ledArray[led_nr][1])); break;
+		case 3: bit_flip(PORTC, BIT(ledArray[led_nr][1])); break;
+		case 4: bit_flip(PORTD, BIT(ledArray[led_nr][1])); break;
+		default: ;
 	}
+	return;
+}
 
-	// turn all 9 LED on/off
-	void led_set_all (uint8_t led_state)
-	{
-		if(led_state){
-			PORTB |= LED_PORTB;
-			PORTD |= LED_PORTD;
-			} else {
-			PORTB &= ~LED_PORTB;
-			PORTD &= ~LED_PORTD;
-		}
-		return;
-	} 
+// turn all 9 LED on/off
+void led_set_all (uint8_t led_state)
+{
+	if(led_state){
+		PORTB |= LED_PORTB;
+		PORTD |= LED_PORTD;
+		} else {
+		PORTB &= ~LED_PORTB;
+		PORTD &= ~LED_PORTD;
+	}
+	return;
+} 
